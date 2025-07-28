6f6ab2cb6429a92bd0bb8c138c9c1d3ab6a4b81d
Morikitan <morikitan25@gmail.com>
2025-07-28 15:40:43 +0900

    ディスプレイ動け

--- lib/display/display.cpp
+++ lib/display/display.cpp
@@ -3,22 +3,24 @@
 #include "hardware/i2c.h"
 #include "hardware/gpio.h"
 #include "pico/stdlib.h"
-
 #include "display.hpp"
 #include "../config.hpp"
 #include "u8g2.h"
 
 
+
 void DisplaySetup(u8g2_t *u8g2){
     // ディスプレイ初期化（I2C + ノーブランドSSD1306用）
-    i2c_init(DisplayI2C, 100 * 1000);  // 400kHz
+    i2c_init(DisplayI2C, 400 * 1000);  // 400kHz
     gpio_set_function(DisplaySDApin, GPIO_FUNC_I2C);  // SDA
     gpio_set_function(DisplaySCLpin, GPIO_FUNC_I2C);  // SCL
+    //gpio_pull_up(DisplaySDApin);
+    //gpio_pull_up(DisplaySCLpin);
     printf("1");
     u8g2_Setup_ssd1306_i2c_128x64_noname_f(
         u8g2, U8G2_R0, u8x8_byte_pico_i2c, u8x8_gpio_and_delay_cb);
     printf("2");
-    u8g2_SetI2CAddress(u8g2, 0x3C << 1); // I2Cアドレス (8bit形式)
+    u8g2_SetI2CAddress(u8g2, 0x78); // I2Cアドレス (8bit形式)
     printf("3");
     u8g2_InitDisplay(u8g2);
     printf("4");
@@ -38,18 +40,10 @@ void UseDisplay(u8g2_t *u8g2){
 // I2C: GP4 = SDA, GP5 = SCL, I2C0使用
 uint8_t u8x8_byte_pico_i2c(u8x8_t *u8g8, uint8_t msg,uint8_t arg_int, void *arg_ptr) {
     switch (msg) {
-        case U8X8_MSG_BYTE_INIT:
-            i2c_init(DisplayI2C, 100 * 1000);  // 400kHz
-            gpio_set_function(DisplaySDApin, GPIO_FUNC_I2C);  // SDA
-            gpio_set_function(DisplaySCLpin, GPIO_FUNC_I2C);  // SCL
-            //gpio_pull_up(DisplaySDApin);
-            //gpio_pull_up(DisplaySCLpin);
-            break;
-
         case U8X8_MSG_BYTE_SEND:
-            i2c_write_blocking(DisplayI2C, 0x3C << 1, (uint8_t *)arg_ptr, arg_int, false);
+            i2c_write_blocking(DisplayI2C, 0x78, (uint8_t *)arg_ptr, arg_int, true);
             break;
-
+        case U8X8_MSG_BYTE_INIT:
         case U8X8_MSG_BYTE_START_TRANSFER:
         case U8X8_MSG_BYTE_END_TRANSFER:
             break;
@@ -60,19 +54,17 @@ uint8_t u8x8_byte_pico_i2c(u8x8_t *u8g8, uint8_t msg,uint8_t arg_int, void *arg_
 // GPIOコールバック（未使用でも定義が必要）
 uint8_t u8x8_gpio_and_delay_cb(u8x8_t *u8x8, uint8_t msg, uint8_t arg_int, void *arg_ptr) {
     switch (msg) {
-        case U8X8_MSG_GPIO_AND_DELAY_INIT:
-            break;
         case U8X8_MSG_DELAY_MILLI:
             sleep_ms(arg_int);
             break;
+        case U8X8_MSG_GPIO_AND_DELAY_INIT:
+        case U8X8_MSG_GPIO_RESET:
+        case U8X8_MSG_GPIO_CS:
+        case U8X8_MSG_GPIO_DC:
         case U8X8_MSG_GPIO_I2C_CLOCK:
-            gpio_put(DisplaySCLpin, arg_int);
-            break;
         case U8X8_MSG_GPIO_I2C_DATA:
-            gpio_put(DisplaySDApin, arg_int);
+            // I2CはGPIO操作不要
             break;
-        default:
-            return 0;
     }
     return 1;
 }
