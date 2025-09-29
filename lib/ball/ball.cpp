81ceda1961d48507d9913018138a4d9132d1a612
Morikitan <morikitan25@gmail.com>
2025-09-29 18:21:43 +0900

    ボールセンサの処理を追加した

--- lib/ball/ball.cpp
+++ lib/ball/ball.cpp
@@ -1,11 +1,19 @@
 #include "ball.hpp"
 #include "../config.hpp"
 #include "pico/stdlib.h"
+#include "math.h"
 #include "hardware/gpio.h"
 #include "hardware/i2c.h"
 
 uint8_t buffer[32];
-uint16_t pulse[16];
+uint16_t BallSensor[16];
+
+bool DoneBallSensor[16];
+float Vector[8];
+int Weight[8];
+int TotalWeight;
+float VectorX,VectorY;
+int VectorNumber;
 
 //ボールセンサーの初期化
 void BallSetup(){
@@ -23,7 +31,96 @@ void UseBallSensor(){
     i2c_read_blocking(BallI2C,0x42,buffer,32,false);
     //データを16bitのもとの形に直す
     for(int i = 0;i < 16;i++){
-        pulse[i] = buffer[i*2] << 8 & 0xFFFF;
-        pulse[i] |= buffer[i * 2 + 1];
+        BallSensor[i] = ((uint16_t)buffer[i*2] << 8) | buffer[i*2 + 1];
     }
+    
+    //データを使える形に変換する
+    for(int i = 0;i < 16;i++){
+      DoneBallSensor[i] = false;
+      if(i < 8){
+        Vector[i] = 0;
+        Weight[i] = 0;
+      }
+    }  
+    VectorNumber = 0;
+    for(int i = 0;i < 16;i++){
+      if(BallSensor[i] > 0 && DoneBallSensor[i] == false){
+        if(i == 0){
+          //LineSensor[0]だけ反時計回り側にあるセンサを考える
+          for(int j = 15;j >= 11;j--){
+            if(BallSensor[j] > 0){
+                DoneBallSensor[j] = true;
+                if(j == 11){
+                    Vector[VectorNumber] -= (16 - j) * 11.25;
+                    Weight[VectorNumber] += 16 - j;
+                }
+            }else{
+                Vector[VectorNumber] -= (15 - j) * 11.25;
+                Weight[VectorNumber] += 15 - j;
+                break;
+            }
+          }
+        }
+        for(int j = 1;j <= 5;j++){
+            //存在しない値を考えないようにする
+            if(i >= 16 - j){
+                Vector[VectorNumber] += (j - 1) * 11.25 + 22.5 * i;
+                Weight[VectorNumber] += j;
+                VectorNumber++;
+                break;
+            }
+            if(BallSensor[i + j] > 0){
+                DoneBallSensor[i + j] = true;
+                if(j == 5){
+                    Vector[VectorNumber] += 56.25 + 22.5 * i;
+                    Weight[VectorNumber] += 6;
+                }
+            }else{
+                Vector[VectorNumber] += (j - 1)*11.25 + 22.5 * i;
+                Weight[VectorNumber] += j;
+                break;
+            }
+        }
+        VectorNumber++;
+        DoneBallSensor[i] = true;
+      }
+    }
+    //ベクトルの合成をする
+    if(SerialWatch == "bal")printf(" ベク ");
+    VectorX = 0;VectorY = 0;TotalWeight = 0;
+    for(int i = 0;i < VectorNumber;i++){
+      VectorX -= sin(Vector[i] / 180.0 * 3.1415) * Weight[i];
+      VectorY += cos(Vector[i] / 180.0 * 3.1415) * Weight[i];
+      if(SerialWatch == "bal"){
+        printf("%d : %f\n",Weight[i],Vector[i]);
+        TotalWeight += Weight[i];
+      }
+    }
+    if(VectorNumber == 0){
+      VectorX = 999;
+      VectorY = 999;
+    }else{
+      VectorX /= (float)TotalWeight;
+      VectorY /= (float)TotalWeight;
+    }
+
+    if(VectorX == 999 && VectorY == 999){
+      BallAngle = 63;
+    }else if(VectorX == 0 && VectorY == 0){
+      BallAngle = 0;
+    }else{
+      if(atan2(VectorY,VectorX) < 1.57){
+        BallAngle = int((1.57 - atan2(VectorY,VectorX)) / 3.1415 * 16);
+      }else{
+        BallAngle = int((7.85 - atan2(VectorY,VectorX)) / 3.1415 * 16);
+      }
+    }
+    //角度の微調整
+    if(BallAngle == 31){
+      BallAngle = 0;
+    }else{
+      BallAngle++;
+    }
+
+    BallAngle *= 11.25;
 }
